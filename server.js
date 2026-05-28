197049182dfa1e5e5bcdcab7e46fcbaaf415b72e
André Abrão <141847383+andreabrao@users.noreply.github.com>
2026-05-28 19:55:08 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -146,14 +146,14 @@ const mimeTypes = {
 };
 
 const modFiles = {
-  "asm-8r": "FS22_asm_8r_performance_br.zip",
-  "case-axial": "FS22_case_axial_flow_9250_br.zip",
-  "nh-t9": "FS22_new_holland_t9_smarttrax_custom.zip",
-  "plantadeira-asm": "FS22_plantadeira_asm_32_linhas.zip",
-  "mapa-sertao": "FS22_mapa_sertao_verde.zip",
-  "script-hud": "FS22_hud_safra_realista.zip",
-  "mf-serie-s": "FS22_massey_serie_s_pro.zip",
-  "grade-asm": "FS22_grade_pesada_asm_48_discos.zip",
+  "asm-8r": "AgroScriptInstaller.exe",
+  "case-axial": "AgroScriptInstaller.exe",
+  "nh-t9": "AgroScriptInstaller.exe",
+  "plantadeira-asm": "AgroScriptInstaller.exe",
+  "mapa-sertao": "AgroScriptInstaller.exe",
+  "script-hud": "AgroScriptInstaller.exe",
+  "mf-serie-s": "AgroScriptInstaller.exe",
+  "grade-asm": "AgroScriptInstaller.exe",
 };
 
 ensureDataFiles();
