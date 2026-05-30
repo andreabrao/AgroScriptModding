35c42bc9740ef26c75dc043bc946db1e216610b2
paroquiasaopedroapostulo <abraosobrado@gmail.com>
2026-05-30 14:14:51 -0300

    Backup automatico - 30/05/2026 14:14:51,12

--- server.js
+++ server.js
@@ -202,25 +202,25 @@ const mimeTypes = {
 };
 
 const modFiles = {
-  "inst-asm-8r":          "Instalador_ASM_8R_PERF_BR.exe",
-  "inst-case-axial":      "Instalador_CASE_AXIAL.exe",
-  "inst-nh-t9":           "Instalador_NH_T9.exe",
-  "inst-plantadeira-asm": "Instalador_PLANTADEIRA_ASM.exe",
-  "inst-mapa-sertao":     "Instalador_MAPA_SERTAO.exe",
-  "inst-script-hud":      "Instalador_SCRIPT_HUD.exe",
-  "inst-mf-serie-s":      "Instalador_MF_SERIE_S.exe",
-  "inst-grade-asm":       "Instalador_GRADE_ASM.exe",
+  "asm-8r":           "Instalador_ASM_8R_PERF_BR.exe",
+  "case-axial":       "Instalador_CASE_AXIAL.exe",
+  "nh-t9":            "Instalador_NH_T9.exe",
+  "plantadeira-asm":  "Instalador_PLANTADEIRA_ASM.exe",
+  "mapa-sertao":      "Instalador_MAPA_SERTAO.exe",
+  "script-hud":       "Instalador_SCRIPT_HUD.exe",
+  "mf-serie-s":       "Instalador_MF_SERIE_S.exe",
+  "grade-asm":        "Instalador_GRADE_ASM.exe",
 };
 
 const modZipFiles = {
-  "asm-8r":               "FS22_ASM_8R_PERF_BR.zip",
-  "case-axial":           "FS22_CASE_AXIAL.zip",
-  "nh-t9":                "FS22_NH_T9.zip",
-  "plantadeira-asm":      "FS22_plantadeira-asm.zip",
-  "mapa-sertao":          "FS22_mapa_sertao.zip",
-  "script-hud":           "FS22_script_hud.zip",
-  "mf-serie-s":           "FS22_mf_serie_s.zip",
-  "grade-asm":            "FS22_grade_asm.zip",
+  "asm-8r":           "FS22_ASM_8R_PERF_BR.zip",
+  "case-axial":       "FS22_CASE_AXIAL.zip",
+  "nh-t9":            "FS22_NH_T9.zip",
+  "plantadeira-asm":  "FS22_plantadeira-asm.zip",
+  "mapa-sertao":      "FS22_mapa_sertao.zip",
+  "script-hud":       "FS22_script_hud.zip",
+  "mf-serie-s":       "FS22_mf_serie_s.zip",
+  "grade-asm":        "FS22_grade_asm.zip",
 };
 
 ensureDataFiles();
