df08990750f9e8bf551a62781e676d7085027285
paroquiasaopedroapostulo <abraosobrado@gmail.com>
2026-05-30 19:38:51 -0300

    Backup automatico - 30/05/2026 19:38:51,32

--- server.js
+++ server.js
@@ -202,15 +202,15 @@ const mimeTypes = {
 };
 
 const modFiles = {
-  "valmt-8750":       "Instalador_valmt-8750.exe",
-  "asm-8r":           "Instalador_asm-8r.exe",
-  "case-axial":       "Instalador_case-axial.exe",
-  "nh-t9":            "Instalador_nh-t9.exe",
-  "plantadeira-asm":  "Instalador_plantadeira-asm.exe",
-  "mapa-sertao":      "Instalador_mapa-sertao.exe",
-  "script-hud":       "Instalador_script-hud.exe",
-  "mf-serie-s":       "Instalador_mf-serie-s.exe",
-  "grade-asm":        "Instalador_grade-asm.exe",
+  "valmt-8750":       "Instalador_VALMT_8750_PERF_BR.exe",
+  "asm-8r":           "Instalador_ASM_8R_PERF_BR.exe",
+  "case-axial":       "Instalador_CASE_AXIAL.exe",
+  "nh-t9":            "Instalador_NH_T9.exe",
+  "plantadeira-asm":  "Instalador_PLANTADEIRA_ASM.exe",
+  "mapa-sertao":      "Instalador_MAPA_SERTAO.exe",
+  "script-hud":       "Instalador_SCRIPT_HUD.exe",
+  "mf-serie-s":       "Instalador_MF_SERIE_S.exe",
+  "grade-asm":        "Instalador_GRADE_ASM.exe",
 };
 
 const modZipFiles = {
@@ -218,11 +218,11 @@ const modZipFiles = {
   "asm-8r":           "FS22_ASM_8R_PERF_BR.zip",
   "case-axial":       "FS22_CASE_AXIAL.zip",
   "nh-t9":            "FS22_NH_T9.zip",
-  "plantadeira-asm":  "FS22_plantadeira-asm.zip",
-  "mapa-sertao":      "FS22_mapa_sertao.zip",
-  "script-hud":       "FS22_script_hud.zip",
-  "mf-serie-s":       "FS22_mf_serie_s.zip",
-  "grade-asm":        "FS22_grade_asm.zip",
+  "plantadeira-asm":  "FS22_PLANTADEIRA_ASM.zip",
+  "mapa-sertao":      "FS22_MAPA_SERTAO.zip",
+  "script-hud":       "FS22_SCRIPT_HUD.zip",
+  "mf-serie-s":       "FS22_MF_SERIE_S.zip",
+  "grade-asm":        "FS22_GRADE_ASM.zip",
 };
 
 ensureDataFiles();
